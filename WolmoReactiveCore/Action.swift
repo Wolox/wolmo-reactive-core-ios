b6237b46fcd06f75b12f89fc01ac15efd04b506e
Diego Quiros <diegoaquiros@gmail.com>
2018-11-23 14:33:22 -0300

    style change

--- WolmoReactiveCore/Action.swift
+++ WolmoReactiveCore/Action.swift
@@ -12,11 +12,11 @@ import ReactiveSwift
 
 extension Action {
     
-    /*
+    /**
      Displays a loading icon in the given view while the action is executing
      
      - parameter view: the view in which the loading icon will be displayed
-    **/
+    */
     public func bindLoading(view: UIView) {
         self.isExecuting.signal.observe(on: UIScheduler()).observeValues { isExecuting in
             if isExecuting {
@@ -27,11 +27,11 @@ extension Action {
         }
     }
     
-    /*
+    /**
      Changes the alpha component of the given button depending on the isEnabled value of the action
      
      - parameter button: the button that should be modified.
-     **/
+     */
     public func bindDisabled(button: UIButton) {
         self.isEnabled.producer.observe(on: UIScheduler()).startWithValues { isEnabled in
             if isEnabled {
